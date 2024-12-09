a78c319badeb4f1dbc10f2140a29d33fea882123
SpackiGabriel <ifprgabriel@gmail.com>
2024-12-09 20:21:13 -0300

    ⚡️ perf(newpso): adjust PSO parameters for efficiency
    - increase cognitive and social factors to 1.5 for improved convergence
    - reduce particle number to 10000 for faster computation

--- newpso.py
+++ newpso.py
@@ -89,8 +89,8 @@ def pso(
 
     # Define os parâmetros do PSO (inércia e fatores de aprendizado).
     w = 0.8 # Peso de inércia.
-    c1 = 1.2 # Fator cognitivo (influência da melhor posição individual).
-    c2 = 1.2 # Fator social (influência da melhor posição do enxame).
+    c1 = 1.5 # Fator cognitivo (influência da melhor posição individual).
+    c2 = 1.5 # Fator social (influência da melhor posição do enxame).
 
     # Iterações para atualizar as partículas.
     for _ in range(iter_n): # Para cada iteração.
@@ -125,7 +125,7 @@ teste = pso(
     [0.271004,1.44862,2.70512,3.94841,5.13112,6.61931,8.60419,11.0863,13.5677,16.068,18.5552,21.0393,23.5223,26.0124,28.4806,30.9418,33.4053,35.8564,38.3088,40.7621,42.843,43.8868,44.409],
     [0.905796,1.98353,2.35874,2.5484,2.67333,2.7765,2.88334,2.9774,3.04683,3.09766,3.14708,3.17517,3.2241,3.24116,3.2549,3.26587,3.27946,3.28687,3.28825,3.28971,3.30512,3.30683,3.30725,],
     obj_func=langmuir,
-    part_n=100000,
+    part_n=10000,
     iter_n=100,
     param_min_max=[[0, 10], [1, 100]],
     comp_n=1,
